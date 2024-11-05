7587f4afb5b62ee251cf79c6eb232386d264b594
rkozhan~ <yalta.vrk@gmail.com>
2024-11-05 17:57:46 +0100

    @click

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 Vue.createApp({
     data: () => ({
         myHtml: '<h1>My Vue 3 App</h1>',
-        title: 'My title',
+        title: 'My items',
         person: {
             firstName: 'John',
             lastName: 'Doe',
             age: 27
-        }
+        },
+        items: [1, 2, 3, 4, 5, 6]
     })
 }).mount('#app')
