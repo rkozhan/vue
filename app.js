7f1fe6be20dad1ba5bd7efb6b05faf0c6e3a2bff
rkozhan~ <yalta.vrk@gmail.com>
2024-11-05 17:35:23 +0100

    directives v-html, v-text, v-pre, v-once

--- app.js
+++ app.js
@@ -1,39 +1,6 @@
-const App = {
-    data() {
-        return {
-            title: 'My notes',
-            myPlaceholder: 'add your note',
-            inputValue: '',
-            notes: ['note 1', 'note 2', 'note 3']
-        }
-    }, 
-    methods: {
-        addNewNote() {
-            if (this.inputValue) {
-                this.notes.push(this.inputValue)
-                this.inputValue = ''
-                
-            }
-        },
-        toUpperCase(str) {
-            return str.toUpperCase()
-        },
-        removeNote(i) {
-            this.notes.splice(i, 1)
-        }
-    },
-    computed: {
-        calcAllNotesComputed() {
-            return this.notes.length
-        }
-    },
-    watch: { 
-        inputValue(value) { 
-            if (value.length > 10) this.inputValue = ''  //some validation
-            console.log('input value changed', value);
-            
-        }
-    }
-}
-
-Vue.createApp(App).mount('#app')
+Vue.createApp({
+    data: () => ({
+        myHtml: '<h1>My Vue 3 App</h1>',
+        title: 'My title'
+    })
+}).mount('#app')
