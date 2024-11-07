3c761ba0e613c9a8d9907fd82082730c5108a3cc
rkozhan~ <yalta.vrk@gmail.com>
2024-11-07 17:49:32 +0100

    Lifecycle Hooks

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+const h = Vue.h
+
 const app = Vue.createApp({
-    template: `
+    data() {
+        return {
+            title: 'this is from template'
+        }
+    },
+/*     template: `
         <div class="card center">
-            <h1>this is from template</h1>
+            <h1>{{ title }}</h1>
+
+            <button class="btn primary" @click="title = 'changed'">Change</button>
         </div>
-    `
+    ` */
+    // how Vue renders Virtual DOM elements
+    render() {
+        return h('div', {
+            class: 'card center'
+        }, 
+        [
+            h('h1', {}, this.title),
+            h('button', {
+                class: 'btn primary',
+                onClick: () => {
+                    this.title = "changed"
+                }
+            }, 'Change')
+        ])
+    }
 })
 
 app.mount('#app')
