feb8c334df9f660774189af8f2882302f258d8e0
rkozhan~ <yalta.vrk@gmail.com>
2024-11-07 18:08:14 +0100

    Create many apps on the same page

--- app.js
+++ app.js
@@ -6,28 +6,24 @@ const app = Vue.createApp({
             title: 'this is from template'
         }
     },
-/*     template: `
+     template: `
         <div class="card center">
             <h1>{{ title }}</h1>
 
             <button class="btn primary" @click="title = 'changed'">Change</button>
         </div>
-    ` */
-    // how Vue renders Virtual DOM elements
-    render() {
-        return h('div', {
-            class: 'card center'
-        }, 
-        [
-            h('h1', {}, this.title),
-            h('button', {
-                class: 'btn primary',
-                onClick: () => {
-                    this.title = "changed"
-                }
-            }, 'Change')
-        ])
-    }
+    ` ,
+    
 })
 
 app.mount('#app')
+
+
+//2nd app
+Vue.createApp({
+    data() {
+        return {
+            title: 'this is second application'
+        }
+    },
+}).mount('#app2')
