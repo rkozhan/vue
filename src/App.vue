3593c55430fb9fd23b6925f64e35353dd60c2c17
rkozhan~ <yalta.vrk@gmail.com>
2024-11-27 11:52:08 +0100

    components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main-header></main-header>
+  <hr>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque reiciendis quia quam, ea modi eum deserunt excepturi quasi. Iusto, consectetur?</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import MainHeader from './MainHeader.vue'
+
+// createApp(App).mount('#app')
+
+const app = createApp(App)
+
+// register a component global
+app.component('main-header', MainHeader)
+
+app.mount('#app')
